5ca0bcd4ebc2632efc85afb32eb57399b085c20b
Daniel Monge <60446165+Danl37@users.noreply.github.com>
2021-12-09 23:54:34 -0800

    Delete Project/Database directory

--- Project/Database/Logs.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Logs]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [timestamp] NCHAR(10) NULL, 
-    [level] NCHAR(10) NULL, 
-    [user] NCHAR(10) NULL, 
-    [category] NCHAR(10) NULL, 
-    [description] NCHAR(10) NULL
-)
