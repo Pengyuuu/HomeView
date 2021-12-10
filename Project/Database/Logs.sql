9cece4d530d90d2bf8592e150be2c8a45fbea982
Dnl37 <dannymonge37@yahoo.com>
2021-12-09 23:14:30 -0800

    rename

--- Project/Database/Logs.sql
+++ Project/Database/Logs.sql
@@ -1,4 +1,9 @@
 ﻿CREATE TABLE [dbo].[Logs]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [timestamp] NCHAR(10) NULL, 
+    [level] NCHAR(10) NULL, 
+    [user] NCHAR(10) NULL, 
+    [category] NCHAR(10) NULL, 
+    [description] NCHAR(10) NULL
 )
