b8b93889ec9ca9c4daec8d5287dfb9f9576c83f7
mlamera <48600479+mlamera@users.noreply.github.com>
2021-12-12 20:40:37 -0800

    Revert "deleted dup project"
    This reverts commit 168a2d03e240a9c4ff3de59cfad7ee47a3ce8fdc.

---
+++ Project/HomeView/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Unite.Homeview
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("more git test");
+        }
+    }
+}
