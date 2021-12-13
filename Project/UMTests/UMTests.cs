b17e82e531ffd57117b5428a48e4cba630d3fd7c
mlamera <48600479+mlamera@users.noreply.github.com>
2021-12-12 19:56:49 -0800

    User UMTests Added

--- Project/UMTests/UMTests.cs
+++ Project/UMTests/UMTests.cs
@@ -1,42 +1,129 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit;
 using System;
-
+using Unite.HomeView.User;
 namespace UMTests
 {
-    [TestClass]
     public class UserTests
     {
-        [Theory]
+        
         public void User_UserShouldBeCreatedAndAddedToDatabase()
         {
-            // Arrange more testing
-
+            // Arrange
+            // String expected = "JSmith";
 
             // Act
-            System.DateTime actualDate = new DateTime(2011, 6, 10);
-            Unite.HomeView.User.User actual = new Unite.HomeView.User.User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, "JSmith");
+            // System.DateTime actualDate = new DateTime(2011, 6, 10);
+            // User actual = new User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, dName: "JSmith");
 
             // Assert
-            string query = 
-            SqlConnection connection = new SqlConnection(@connectionString);
-            SqlCommand command = new SqlCommand(query, connection);
-            try
-            {
-                connection.Open();
-                command.ExecuteNonQuery();
-                Console.WriteLine("Records Inserted Successfully");
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine("Error Generated. Details: " + e.ToString());
-                success = false;
-            }
-            finally
-            {
-                connection.Close();
-            }
+            // Connect to DB and query to find "JSmith" Display Name
 
+        }
+
+        [Fact]
+        public void User_GetFirstShouldReturnFirstName()
+        {
+            // Arrange
+            String expected = "John";
+
+            // Arrange
+            System.DateTime actualDate = new DateTime(2011, 6, 10);
+            User actual = new User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, dName: "JSmith");
+
+
+            // Assert
+            Assert.Equal(expected, actual.getfirst());
+
+        }
+
+        [Fact]
+        public void User_GetLastShouldReturnLastName()
+        {
+            // Arrange
+            String expected = "Smith";
+
+            // Arrange
+            System.DateTime actualDate = new DateTime(2011, 6, 10);
+            User actual = new User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, dName: "JSmith");
+
+
+            // Assert
+            Assert.Equal(expected, actual.getlast());
+        }
+
+        [Fact]
+        public void User_GetEmailShouldReturnEmail()
+        {
+            // Arrange
+            String expected = "JohnSmith@gmail.com";
+
+            // Arrange
+            System.DateTime actualDate = new DateTime(2011, 6, 10);
+            User actual = new User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, dName: "JSmith");
+
+
+            // Assert
+            Assert.Equal(expected, actual.getemail());
+        }
+
+        [Fact]
+        public void User_GetPWShouldReturnPassword()
+        {
+            // Arrange
+            String expected = "abc123";
+
+            // Arrange
+            System.DateTime actualDate = new DateTime(2011, 6, 10);
+            User actual = new User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, dName: "JSmith");
+
+
+            // Assert
+            Assert.Equal(expected, actual.getpw());
+        }
+
+        [Fact]
+        public void User_GetDOBShouldReturnDateOfBirth()
+        {
+            // Arrange
+            System.DateTime expected = new DateTime(2011, 6, 10);
+
+            // Arrange
+            System.DateTime actualDate = new DateTime(2011, 6, 10);
+            User actual = new User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, dName: "JSmith");
+
+
+            // Assert
+            Assert.Equal(expected, actual.getdob());
+        }
+
+        [Fact]
+        public void User_GetDispShouldReturnDisplayName()
+        {
+            // Arrange
+            String expected = "JSmith";
+
+            // Arrange
+            System.DateTime actualDate = new DateTime(2011, 6, 10);
+            User actual = new User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, dName: "JSmith");
+
+
+            // Assert
+            Assert.Equal(expected, actual.getdisp());
+        }
+
+        [Fact]
+        public void User_GetRegShouldReturnRegistrationDate()
+        {
+            // Arrange
+            System.DateTime expected = DateTime.UtcNow;
+
+            // Arrange
+            System.DateTime actualDate = new DateTime(2011, 6, 10);
+            User actual = new User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, dName: "JSmith");
+
+
+            // Assert
+            Assert.Equal(expected, actual.getreg());
         }
     }
 }
