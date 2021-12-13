860f154e0b9eef68c79d6918b7516b5fbbc6b0a0
mlamera <48600479+mlamera@users.noreply.github.com>
2021-12-12 18:16:00 -0800

    UMTest Update

---
+++ Project/Homeview/User/UserManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+//using UserManagement.User;
+
+
+/* User Authentication and Authorization Manager */
+namespace Unite.HomeView.User {
+	class UserManager
+	{
+		private String admin = "TeamUnite";
+		private String adminpw = "Testing";
+		private UMService = umservice;
+
+	public UserManager()
+		{
+			umService = new UMService();
+		}
+
+		/* Verifies if actor is system administrator */
+		public Boolean verifyAdmin()
+		{
+			Console.WriteLine("Enter username:");
+			string adminUsername = Console.ReadLine();
+			Console.WriteLine("Enter password:");
+			string pw = Console.ReadLine();
+			Boolean check1 = adminUsername == this.admin ? true : false;
+			Boolean check2 = adminpw == this.adminpw ? true : false;
+			return check1 == check2;
+
+		}
+
+		/* Ensures that New user has entered correct fields 
+		 *	Checks for valid email address, valid password, valid username */
+		public Boolean checkNewUser(User u)
+		{
+			string email = u.getemail();
+			Boolean emailCheck = (email.Contains("@") == email.Contains(".com"));
+
+			string pw = u.getpw();
+			int pwMinLength = 12;
+			int fields = 3;     // must have valid at least 12 char, at least 1 upper char, at least 1 non-alphanum char
+			Boolean containsUpper = false;
+			Boolean containsNonAlpha = false;
+			Boolean lengthCheck = false;
+
+			if (pw.Length >= pwMinLength)
+			{
+				lengthCheck = true;
+			}
+
+			for (int i = 0; i < pw.Length; i++)
+			{
+				if (Char.IsUpper(pw[i]))
+				{
+					containsUpper = true;
+				}
+
+				if (!Char.IsLetterOrDigit(pw[i]))
+				{
+					containsNonAlpha = true;
+				}
+			}
+
+			return (emailCheck == containsUpper == containsNonAlpha == lengthCheck);
+
+		}
+
+
+		public String UserManagerCreateUser(User u)
+		{
+			Boolean adminCheck = verifyAdmin();
+			Boolean newuserCheck = checkNewUser();
+
+			if (!adminCheck)
+			{
+				return "Unauthorized access.";
+			}
+			if (!newuserCheck)
+			{
+				return "Invalid inputs for new user.";
+			}
+
+			string m = this.umService.UMServiceCreateUser(u) == true ? "User account record creation successful." : "Account creation unsuccessful. Account already exists in system. ";
+			return m;
+		}
+
+		/* Modifies a user record in the system 
+		 * 2 = Update information
+		 * 3 = Delete account
+		 * 4 = Disable
+		 * 5 = Enable
+		 */
+		public String UserManagerModifyUser(User u, int mode)
+		{
+			Boolean adminCheck = verifyAdmin();
+
+			if (!adminCheck)
+			{
+				return "Unauthorized access.";
+			}
+
+
+
+			string m = this.umService.UMServiceCreateUser(u) == true ? "User account record creation successful." : "Account creation unsuccessful. Account already exists in system. ";
+			return m;
+		}
+	}
+}
--- Project/UMTests/UMTests.cs
+++ Project/UMTests/UMTests.cs
@@ -1,13 +1,42 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using System;
 
 namespace UMTests
 {
     [TestClass]
-    public class UnitTest1
+    public class UserTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Theory]
+        public void User_UserShouldBeCreatedAndAddedToDatabase()
         {
+            // Arrange
+
+
+            // Act
+            System.DateTime actualDate = new DateTime(2011, 6, 10);
+            Unite.HomeView.User.User actual = new Unite.HomeView.User.User("John", "Smith", "JohnSmith@gmail.com", "abc123", actualDate, "JSmith");
+
+            // Assert
+            string query = 
+            SqlConnection connection = new SqlConnection(@connectionString);
+            SqlCommand command = new SqlCommand(query, connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("Records Inserted Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+                success = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
         }
     }
 }
