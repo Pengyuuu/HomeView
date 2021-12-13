deae1271efed4f36d1f0c901b09027e6d48b73d6
mlamera <48600479+mlamera@users.noreply.github.com>
2021-12-12 20:39:15 -0800

    Revert "Merge branch 'master' of https://github.com/Pengyuuu/HomeView"
    This reverts commit a41d0cc175b9d65ff751572a1557099462bda346, reversing
    changes made to f893525d98f689fd107f582ee4c493f16b1e02a6.

--- Project/Homeview/Logging/LoggingService.cs
+++ Project/Homeview/Logging/LoggingService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Unite.HomeView.Logging
 {
-    class LoggingService : Contracts.ILogService
+    class LoggingService : ILogService
     {
         // Creates a log object
         private Log log;
@@ -35,13 +35,11 @@ namespace Unite.HomeView.Logging
         }
 
         // Method to create log 
-        public void Log(int id, string userop, string desc, LogLevel level, LogCategory category, DateTime timestamp)
+        public void Log(int id, string desc, string mycol, string timestamp)
         {
             log.Id = id;
-            log.UserOperation = userop;
             log.Description = desc;
-            log.Level = level;
-            log.Category = category;
+            log.Mycol = mycol;
             log.Timestamp = timestamp;
         }
     }
