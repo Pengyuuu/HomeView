0caed3d58ea27aa2b0892fa69d5b1da0ff0b42e3
Eric Truong <erictruong15@gmail.com>
2021-12-12 19:19:36 -0800

    Log file and Logging Service

---
+++ Project/Homeview/Logging/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unite.HomeView.Logging
+{
+    class Log
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Mycol { get; set; }
+
+        // I don't know what datatype timestamp is supposed to be
+        public string Timestamp { get; set; }
+    }
+}
