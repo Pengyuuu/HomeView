1b3b9e180f5b88781b699752fa1955e693ce8da3
erinalara <48671321+erinalara@users.noreply.github.com>
2021-12-13 19:27:09 -0800

    Adjusted Demo

--- Project/Demo/Demo.cs
+++ Project/Demo/Demo.cs
@@ -10,7 +10,7 @@ namespace Demo
         {
             Console.WriteLine("HomeView Milestone 3 Demo\n\n");
             Console.WriteLine(ConfigurationManager.ConnectionStrings["connString"].ConnectionString);
-            User u = new User("marsellus", "wallace", "mWallace@pulp.com", "iL0vem1@12345",new DateTime(2000, 12, 12), "mWallace");
+            User u = new User("marsellus", "wallace", "mWallace@pulp.com", "iL0vem1@12345",new DateTime(2000, 12, 12), "mWallace", Role.2);
             UserManager um = new UserManager();
             um.UserManagerCreateUser("TeamUnite", "Testing", u);
         }
