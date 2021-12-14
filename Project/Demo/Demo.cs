8b178658617b39f05f2ab2ec1997fe3e92cf681b
mlamera <48600479+mlamera@users.noreply.github.com>
2021-12-14 03:27:49 -0800

    Changed configuration manager to 5.0.0

--- Project/Demo/Demo.cs
+++ Project/Demo/Demo.cs
@@ -12,8 +12,8 @@ namespace Demo
             Console.WriteLine("HomeView Milestone 3 Demo\n\n");
             //User u = new User("marsellus", "wallace", "mWallace@pulp.com", "iL0vem1@12345",new DateTime(2000, 12, 12), "mWallace", Role.User);
             User u = new User("vincent", "vega", "vvega@pulp.com", "iL0vem1@12345", new DateTime(2000, 12, 12), "vvega", Role.User);
-            UserManager um = new UserManager();
-            um.UserManagerCreateUser("TeamUnite", "Testing", u);
+            UserManager um = new UserManager("TeamUnite", "Testing");
+            um.UserManagerCreateUser(u);
 
             
         }
