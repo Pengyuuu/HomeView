50cf565506e0dd1338c4c7e660381c912d24a8a9
Dnl37 <dannymonge37@yahoo.com>
2021-12-14 01:54:51 -0800

    added ConnectionString

---
+++ Project/Data/ConnectionString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Data
+{
+    class ConnectionString
+    {
+        static string connStr;
+        static string getConnectionString()
+        {
+            if (String.IsNullOrEmpty(connStr))
+            {
+                string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string path = (System.IO.Path.GetDirectoryName(executable));
+                path = Path.GetFullPath(Path.Combine(path, "@\\..\\..\\..\\Homeview.mdf"));
+                connStr = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =" + path + "; Integrated Security = True; Connect Timeout = 30";
+            }
+            return connStr;
+        }
+
+    }
+}
