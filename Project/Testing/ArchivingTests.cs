d3de615827e0fcdb3b9fb553e8089cc3631f0fb5
mlamera <48600479+mlamera@users.noreply.github.com>
2021-12-15 18:45:06 -0800

    ArchivingTests created

--- Project/Testing/ArchivingTests.cs
+++ Project/Testing/ArchivingTests.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Archive;
+using Logging.Logging;
 
 namespace Testing
 {
-    class ArchivingTests
+    public class ArchivingTests
     {
+        [Fact]
+        public void Archiving_ArchivingOldLogShouldLogLogsOlderThan30Days()
+        {
+            Boolean expected = true;
+
+            Log testLog = new("ArchivingTests - Log", LogLevel.Info, LogCategory.Data, new DateTime(2020, 10, 10));
+            LoggingManager logManager = new LoggingManager();
+            logManager.logData(testLog);
+
+            Archive.Archiving archivingTest = new Archive.Archiving();
+            Boolean actual = archivingTest.oldLog();
+
+            Assert.Equal(expected, actual);
+
+        }
     }
 }
