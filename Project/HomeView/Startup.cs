81b25fc1a4c4948e5bc40fedb26b53c4734c8112
Dnl37 <dannymonge37@yahoo.com>
2022-02-20 04:12:33 -0800

    Updated homepage

--- Project/HomeView/Startup.cs
+++ Project/HomeView/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Registration
+namespace HomeView
 {
     public class Startup
     {
--- Project/Testing/LoggingTests.cs
+++ Project/Testing/LoggingTests.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Xunit;
 using Logging.Logging;
 
-namespace Testing.LoggingTests
+namespace LoggingTests
 {
     public class LoggingTests
 
