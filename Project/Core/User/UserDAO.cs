11ac52a2432871672885ffe882ba4f288ea51aaf
Dnl37 <dannymonge37@yahoo.com>
2022-02-21 01:12:49 -0800

    UM updated

--- Project/Core/User/UserDAO.cs
+++ Project/Core/User/UserDAO.cs
@@ -11,69 +11,41 @@ using Data;
 namespace Core.User
 {
     public class UserDAO
-{
-
-    private readonly SqlDataAccess _db;
-
-    public UserDAO(SqlDataAccess db)
     {
-        _db = db;
-    }
 
-    public Task CreateUser(User user)
-    {
-        int userR = (int)user.UserRole;
-        return _db.SaveData("dbo.InsertUser", new { user.FirstName, user.LastName, user.UserEmail, user.UserPassword, user.UserDob, user.DispName, user.RegDate, user.UserStatus, userR });
-    }
+        private readonly SqlDataAccess _db;
 
-    public Task UpdateUser(User user)
-    {
+        public UserDAO(SqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task CreateUser(User user)
+        {
             int userR = (int)user.UserRole;
+            return _db.SaveData("dbo.InsertUser", new { user.FirstName, user.LastName, user.UserEmail, user.UserPassword, user.UserDob, user.DispName, user.RegDate, user.UserStatus, userR });
+        }
 
+        public Task UpdateUser(User user)
+        {
+            int userR = (int)user.UserRole;
             return _db.SaveData("dbo.UpdateUser", new { firstN = user.FirstName, user.LastName, user.UserEmail, user.UserPassword, user.UserDob, user.DispName, user.UserStatus, userR });
-    }
-
-    public Task<IEnumerable<User>> ReadUser(string email)
-    {
-            Task<IEnumerable<User>> results;
-        
-            try
-            {
-                results = _db.LoadData<User, dynamic>("dbo.GetUser", new { email = email});
-            }
-            catch (Exception e)
-            {
-                results = null;
-            }
-            return results;
-    }
-
-    public Task<IEnumerable<User>> ReadUser()
-    {
-            
-            Task<IEnumerable<User>> results;
-            try
-            {
-                results = _db.LoadData<User, dynamic>("dbo.GetAllUsers", "");
-            }
-            catch (Exception e)
-            {
-                results = null;
-            }
-            return results;
         }
 
-    public Boolean DeleteUser(User user)
-    {
-            try
-            {
-                var result = _db.SaveData("dbo.DeleteUser", user.UserEmail);
-            }
-            catch (Exception e)
-            {
-                return false;
-            }
-            return true;
+        public async Task<User?> ReadUser(string email)
+        {
+            var results = await _db.LoadData<User, dynamic>("dbo.GetUser", new { email = email});
+            return results.FirstOrDefault();
         }
-}
+
+        public Task<IEnumerable<User>> ReadAllUsers()
+        {
+            return _db.LoadData<User, dynamic>("dbo.GetAllUsers", "");
+        }
+
+        public Task DeleteUser(string email)
+        {
+            return _db.SaveData("dbo.DeleteUser", email);
+        }
+    }
 }
