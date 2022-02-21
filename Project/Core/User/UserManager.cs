7e25d60f6514b7bdcab9529f2590bce111a33f4e
mlamera <48600479+mlamera@users.noreply.github.com>
2022-02-20 23:44:29 -0800

    User Manager, Get/Delete/Modify User

--- Project/Core/User/UserManager.cs
+++ Project/Core/User/UserManager.cs
@@ -3,6 +3,9 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using Logging;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Data;
 
 
 /* User Authentication and Authorization Manager */
@@ -20,50 +23,6 @@ namespace Core.User
 		}
 
 
-
-		/* Ensures that New user has entered correct fields 
-		 *	Checks for valid userEmail address, valid password */
-		public Boolean IsValidUser(User u)
-		{
-			Log userLog = new("checking user in database", LogLevel.Info, LogCategory.Data, DateTime.Now);
-			LoggingManager logManager = new();
-			logManager.LogData(userLog);
-
-			//  makes sure new user's userEmail is valid (contains @.com)
-			string userEmail = u.UserEmail;
-			Boolean isValidEmail = (userEmail.Contains("@") == userEmail.Contains(".com"));
-
-			// makes sure new user's password is valid (contains minimum of 12 characters, at least 1 capital letter, at least 1 non-alphanumeric character
-			string userPass = u.UserPassword;
-			int PASS_MIN_LENGTH = 12;
-			Boolean containsUpper = false;
-			Boolean containsNonAlpha = false;
-			Boolean hasLength = false;
-
-			// ensures password meets length requirement
-			if (userPass.Length >= PASS_MIN_LENGTH)
-			{
-				hasLength = true;
-			}
-
-			// ensures pasword meets capital and non-alphanumeric requirement
-			for (int i = 0; i < userPass.Length; i++)
-			{
-				if (Char.IsUpper(userPass[i]))
-				{
-					containsUpper = true;
-				}
-
-				if (!Char.IsLetterOrDigit(userPass[i]))
-				{
-					containsNonAlpha = true;
-				}
-			}
-
-			return (isValidEmail == containsUpper == containsNonAlpha == hasLength);
-
-		}
-
 		// Gets user
 		public User GetUser(string email)
         {
@@ -73,16 +32,37 @@ namespace Core.User
 			return u;
         }
 
-		public String GetAllUsers()
+		// Get all users (Return as string or list?)
+		public IEnumerable<User> GetAllUsers()
 		{
-			Log userLog = new();
-			LoggingManager logManager = new();
+			IEnumerable<User> u = _userDAO.ReadUser().Result;
+			Log userLog = new("All users retrieved.", LogLevel.Info, LogCategory.DataStore, DateTime.Now);
+			_loggingManager.LogData(userLog);
+			return u;
+		}
 
-			userLog = new("Fetching all users.", LogLevel.Info, LogCategory.DataStore, DateTime.Now);
-			logManager.LogData(userLog);
+		public bool DeleteUser(User user)
+        {
+			bool result = _userDAO.DeleteUser(user);
 
-			var result = this._userDAO.ReadUser();			
-			return result.ToString();
+			if (result)
+            {
+				Log userLogTrue = new("User: " + user.UserEmail + " - successfully deleted.", LogLevel.Info, LogCategory.DataStore, DateTime.Now);
+				_loggingManager.LogData(userLogTrue);
+				return result;
+            }
+
+			Log userLogFalse = new("User: " + user.UserEmail + " - unsuccessful delete user.", LogLevel.Error, LogCategory.DataStore, DateTime.Now);
+			_loggingManager.LogData(userLogFalse);
+			return result;
+        }
+
+		// What should we return here
+		public User ModifyUser(User user)
+        {
+			Task t = _userDAO.UpdateUser(user);
+			Log userLog = new("User updated.", LogLevel.Info, LogCategory.DataStore, DateTime.Now);
+			_loggingManager.LogData(userLog);
 
 		}
 
@@ -121,68 +101,9 @@ namespace Core.User
 			return "User data successfully exported to .csv file";
 		}
 
-		public String CreateUser(User userMod)
-		{
-			Log userLog = new();
-			LoggingManager logManager = new();
-			// if user is not admin, returns unauthorized access
-			if (!this._isVerified)
-			{
-				userLog = new("Unauthorized admin access.", LogLevel.Error, LogCategory.View, DateTime.Now);
-				logManager.LogData(userLog);
-				return "Unauthorized access.";
-			}
 
-			if (this._userDAO.ReadUser(userMod.UserEmail) is null);
-			{
-				userLog = new("User already exists", LogLevel.Error, LogCategory.View, DateTime.Now);
-				logManager.LogData(userLog);
-				return "User already exists";
-			}
 
-			userLog = new("Creating user", LogLevel.Info, LogCategory.DataStore, DateTime.Now);
-			logManager.LogData(userLog);
-			// calls service layer to modify user
 
-			return "not implemented";
-		}
-
-		/* Modifies a user record in the system 
-		 * Returns a success or unsuccessful message
-		 */
-		public String ModifyUser(User userMod)
-		{
-			Log userLog = new();
-			LoggingManager logManager = new();
-			// if user is not admin, returns unauthorized access
-			if (!this._isVerified)
-			{
-				userLog = new("Unauthorized admin access.", LogLevel.Error, LogCategory.View, DateTime.Now);
-				logManager.LogData(userLog);
-				return "Unauthorized access.";
-			}
-
-			userLog = new("Modifying user", LogLevel.Info, LogCategory.DataStore, DateTime.Now);
-			logManager.LogData(userLog);
-			// calls service layer to modify user
-
-			return "not impl";
-		}
-
-		public String DeleteUser(User userMod)
-		{
-			Log userLog = new();
-			LoggingManager logManager = new LoggingManager();
-			// if user is not admin, returns unauthorized access
-			if (!this._isVerified)
-			{
-				userLog = new("Unauthorized admin access.", LogLevel.Error, LogCategory.View, DateTime.Now);
-				logManager.LogData(userLog);
-				return "Unauthorized access.";
-			}
-
-			return "na";
-		}
 
 		// Bulk operation
 		/* Modifies a user record in the system 
