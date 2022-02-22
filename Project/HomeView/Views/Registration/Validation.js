164f2aaaabea1e4c8b7fd850e9a163a1df8738d8
Christian <ctlam0513@gmail.com>
2022-02-21 18:53:57 -0800

    attempt of validation

---
+++ Project/HomeView/Views/Registration/Validation.js
@@ -0,0 +1,13 @@
+﻿const firstName = document.getElementById('firstName')
+const lastName = document.getElementById('lastName')
+const form = document.getElementById('form')
+
+form.addEventListener('SignUp', (e) => {
+    let messages = [];
+    if (firstName.value == '' || firstName == null) {
+        messages.push("Required Field");
+    }
+
+    if (message.length > 0) {
+
+    }
