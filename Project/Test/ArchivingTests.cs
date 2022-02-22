c107681dd6e4e44796b7ba7ffd08b066d0348c45
mlamera <48600479+mlamera@users.noreply.github.com>
2022-02-21 18:30:51 -0800

    Archive compression

--- Project/Test/ArchivingTests.cs
+++ Project/Test/ArchivingTests.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Archive;
 
 
 namespace ArchivingTesting
 {
     public class ArchivingTests
     {
-
+        [Fact]
+        public void ArchivingManager_Send()
+        {
+            bool expected = true;
+            Archived test = new Archived();
+            bool actual = test.ArchiveLog();
+            Assert.Equal(expected, actual);
+        }
     }
 }
