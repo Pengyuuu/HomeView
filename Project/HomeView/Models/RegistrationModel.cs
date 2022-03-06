9883ed549464d10ae38a2082eb81444974970c07
mlamera <48600479+mlamera@users.noreply.github.com>
2022-03-06 02:36:38 -0800

    Registration, password validation and email validation

--- Project/HomeView/Models/RegistrationModel.cs
+++ Project/HomeView/Models/RegistrationModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.User;
+using HomeView.Utilities;
 
 namespace HomeView.Models
 {
@@ -11,17 +12,26 @@ namespace HomeView.Models
     {
         [Required(ErrorMessage ="First name is required. ")]
         public string _firstName { get; set; }
+
         [Required(ErrorMessage = "Last name is required. ")]
         public string _lastName { get; set; }
+        
+
         [Required(ErrorMessage = "Email is required. ")]
         [EmailAddress(ErrorMessage ="Email must be a valid email address.")]
+        [ValidNewEmail(ErrorMessage = "Email already in use.")]
         public string _userEmail { get; set; }
+
+        // Passwords must have a minimum of 12 characters with one capital letter and one non-alphanumeric character
+        [RegularExpression("^(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{12,}$",
+            ErrorMessage = "Invalid Password. Must include: <br>• 12 Characters minimum <br>• 1 Capital letter minimum<br>• 1 Special character minimum")]
         [Required(ErrorMessage = "Password is required. ")]
-        [StringLength(50, ErrorMessage= "Password must be at least 12 characters long. ", MinimumLength = 12)]
         public string _userPass { get; set; }
+
         [Required(ErrorMessage = "Date Of Birth is required. ")]
         [DataType(DataType.Date)]
         public DateTime _userDob { get; set; }
+
         [Required(ErrorMessage = "Display name is required. ")]
         [StringLength(20, ErrorMessage="Display name must be at least 10 characters long. ", MinimumLength = 10)]
         public string _dispName { get; set; }
---
+++ Project/HomeView/Utilities/ValidNewEmailAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Core.User;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeView.Utilities
+{
+    public class ValidNewEmailAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                string newEmail = value.ToString();
+                //Console.WriteLine(newEmail);
+                UserManager userManager = new UserManager();
+                //string testUser = userManager.GetUser(newEmail).Email;
+                
+                if (userManager.GetUser(newEmail) == null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
