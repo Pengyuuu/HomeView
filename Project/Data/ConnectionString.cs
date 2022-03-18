3e47d13ad2b2ee6ba32f7aad3aba07b93b3cf224
erinalara <48671321+erinalara@users.noreply.github.com>
2022-03-18 04:01:36 -0700

    made rds (database) for deployment

--- Project/Data/ConnectionString.cs
+++ Project/Data/ConnectionString.cs
@@ -13,7 +13,10 @@ namespace Data
                 string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
                 string path = (System.IO.Path.GetDirectoryName(executable));
                 path = Path.GetFullPath(Path.Combine(path, "@\\..\\..\\..\\..\\..\\..\\Project\\Data\\Database\\Homeview.mdf"));
-                connStr = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =" + path + "; Integrated Security = True; Connect Timeout = 30";
+                //connStr = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =" + path + "; Integrated Security = True; Connect Timeout = 30";
+                string sqlAuthpw = "homeviewpass";
+                connStr = "Data Source=homeviewdb.cotk9avwlowj.us-west-1.rds.amazonaws.com,1433;Initial Catalog=homeviewDB;Persist Security Info=True;User ID=awshomeviewadmin;Password="+ sqlAuthpw;
+
             }
             return connStr;
         }
