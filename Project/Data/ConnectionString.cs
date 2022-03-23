939f436cf5344b551629733bfb5966ae1e4488ec
erinalara <erina.lara@student.csulb.edu>
2022-03-23 16:40:06 -0700

    pls we are just trying to make our senior proejct

--- Project/Data/ConnectionString.cs
+++ Project/Data/ConnectionString.cs
@@ -14,7 +14,6 @@ namespace Data
                 string path = (System.IO.Path.GetDirectoryName(executable));
                 path = Path.GetFullPath(Path.Combine(path, "@\\..\\..\\..\\..\\..\\..\\Project\\Data\\Database\\Homeview.mdf"));
                 //connStr = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =" + path + "; Integrated Security = True; Connect Timeout = 30";
-                connStr = "Data Source=homeviewdb.cotk9avwlowj.us-west-1.rds.amazonaws.com,1433;Initial Catalog=homeviewDB;Persist Security Info=True;User ID=awshomeviewadmin;Password="+ sqlAuthpw;
 
             }
             return connStr;
