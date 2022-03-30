dabb2214c025ea1e6f1e240a7c0fc84be54cbe76
mlamera <48600479+mlamera@users.noreply.github.com>
2022-03-30 01:53:18 -0700

    basic log in / reg pages

---
+++ Project/homeview-frontend/src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Register from "./Register"
+import {Container} from 'react-bootstrap'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import Login from './Login'
+
+function App() {
+  return (
+  <Container className="d-flex align-items-center
+   justify-content-center"
+   style={{minHeight:"100vh"}}>
+     <div className="w-100" style={{maxWidth:"400px"}}>
+     <Router>
+       <Routes>
+         <Route exact path="/" element={<Home/>}/>
+         <Route exact path="/register" element={<Register/>} />
+         <Route exact path="/login" element={<Login/>} />
+       </Routes>
+     </Router>
+     </div>
+  </Container>
+  )
+}
+
+export default App;
