5a4de3ddf9a888a21c69ece5a486770595774bb1
erinalara <48671321+erinalara@users.noreply.github.com>
2022-04-10 21:46:01 -0700

    working on api controllers

--- Project/HomeView-API/Controllers/RegistrationController.cs
+++ Project/HomeView-API/Controllers/RegistrationController.cs
@@ -1,28 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+using Managers.Contracts;
+using System.Web.Http;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HomeView_API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RegistrationController : ControllerBase
+    [Route("/registration")]
+    public class RegistrationController : ApiController
     {
-        // GET: api/<RegistrationController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly IRegistrationManager _registrationManager;
+        private readonly HttpClient _httpClient;
+
+
+        public RegistrationController(IRegistrationManager registrationManager)
         {
-            return new string[] { "value1", "value2" };
+            _registrationManager = registrationManager;
+            _httpClient = new HttpClient();
         }
 
-        // GET api/<RegistrationController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+
+        // GET /registration/email/dob/pw
+        [HttpGet("{email}/{dob}/{pw}")]
+        public IHttpActionResult Get(string email, string dob, string pw)
         {
-            return "value";
+            HttpResponse response = null;   // sorta confused on this part
+            
+            bool isValid = _registrationManager.ValidateFields(email, dob, pw);
+            if (isValid)
+            {
+                _registrationManager.CreateUser(email, dob, pw);
+                response.StatusCode = 200;
+
+            }
+            else
+            {
+                response.StatusCode = 400;
+            }
+            return response;
         }
 
-        // POST api/<RegistrationController>
+        // POST /registration
         [HttpPost]
         public void Post([FromBody] string value)
         {
