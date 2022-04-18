b4ce7c790502a95196048c931a71bebd74d4ce3b
erinalara <48671321+erinalara@users.noreply.github.com>
2022-04-17 21:57:35 -0700

    taking out unused usings

--- Project/Managers/Implementations/EmailManager.cs
+++ Project/Managers/Implementations/EmailManager.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Services.Contracts;
 using System.Net.Mail;
 using Services.Implementations;
