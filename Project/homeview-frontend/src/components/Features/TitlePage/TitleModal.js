98758bed2df4cae9346f3e58f5a12e0a644d219a
erinalara <48671321+erinalara@users.noreply.github.com>
2022-04-19 15:57:29 -0700

    pushing modal, might need to debug closing modal

--- Project/homeview-frontend/src/components/Features/TitlePage/TitleModal.js
+++ Project/homeview-frontend/src/components/Features/TitlePage/TitleModal.js
@@ -1,28 +1,30 @@
 import React, { useState } from 'react'
-import '../../../css/test.css'
+import '../../../css/App.css'
 import RatingReview from './RatingReview'
 import { Button, Image, Modal } from 'react-bootstrap'
 import { render } from 'react-dom'
 
+const IMG_API = "https://image.tmdb.org/t/p/original/"
+
 export default function TitleModal(props) {
-    console.log(props.props.title);
+    console.log(props);
     return (
         <Modal {...props} aria-labelledby="contained-modal-title-vcenter" size="lg">
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    {props.props.title}
+                    {props.title}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body  >
-                <div>
-                    <img className="title-container" src={props.props.imgsrc}></img>
+                <div>image
+                    <img className="title-container" src={IMG_API + props.poster}></img>
                 </div>
                 <div className="title-details">
-                <p >{props.props.year}</p>
-                <p>{props.props.hvRating}</p>
-                <p>{props.props.services}</p>
-                <p>{props.props.genres}</p>
-                    <p>{props.props.actors}</p>
+                <p> props.props.year</p>
+                <p>props.props.hvRating</p>
+                <p>props.props.services</p>
+                <p>props.props.genres</p>
+                    <p>props.props.actors</p>
                     </div>
             </Modal.Body>
             <Modal.Footer>
--- Project/homeview-frontend/src/components/cml/MovieTile.js
+++ Project/homeview-frontend/src/components/cml/MovieTile.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TitleModal from '../Features/TitlePage/TitleModal';
 import './../../css/movietile.css';
+
 
 const IMG_API = "https://image.tmdb.org/t/p/original/"
 
-const Movie = ({title, posterPath, overview}) => (
-
-<div className='movie'>
-        <img src={IMG_API + posterPath} alt={title}/>
-        <div className='img-overlay img-overlay--blur'>
-            <div className="movie-title">{title}</div>
-            <p className='overview'>{overview}</p>
+const Movie = ({ title, posterPath, overview }) => {
+    const [modalShow, setModalShow] = useState(false);
+    console.log(title, posterPath, overview);
+    return[
+    
+        <div className='movie' onClick={() => setModalShow(true)}>
+            <img src={IMG_API + posterPath} alt={title} />
+            <div className='img-overlay img-overlay--blur'>
+                <div className="movie-title">{title}</div>
+                <p className='overview'>{overview}</p>
+            </div>
+            <TitleModal title={title} poster={posterPath} overview={overview} show={modalShow} onHide={() => setModalShow(false)}
+            />
         </div>
-    </div>
-)
+    ]
+}
+
 
 export default Movie;
