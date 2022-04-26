79648c4a6adff0fb5796dad0a40dc4499348ce6b
Pengyuuu <erictruong15@gmail.com>
2022-04-25 17:44:42 -0700

    Playlist controller

--- Project/Managers/Contracts/IPlaylistManager.cs
+++ Project/Managers/Contracts/IPlaylistManager.cs
@@ -6,6 +6,7 @@ namespace Managers.Contracts
 {
     public interface IPlaylistManager
     {
+        bool ValidateName(string playlistName, string email);
         bool CreatePlaylist(string playlistName, string email, PlaylistViewMode viewMode);
 
         bool DeletePlaylist(string playlistName, string email);
