ffc3bc964c99a01fdc661a0536ba7c4fc8d11aa1
Christian <ctlam0513@gmail.com>
2022-04-27 14:42:45 -0700

    act update

---
+++ Project/HomeView-API/Controllers/ActWikiController.cs
@@ -0,0 +1,17 @@
+usingusing Microsoft.AspNetCore.Mvc;
+using Features.Ratings_and_Reviews;
+using Managers.Contracts;
+using Managers.Implementations;
+
+namespace HomeView_API.Controllers
+{
+    public class ActWikiController : Controller
+    {
+        private readonly ActWikiManager _actManager;
+        
+        public ActWikiController()
+        {
+            _actManager = new ActWikiManager();
+        }
+    }
+}
