0f5652b135b4d1a580d00646daf4499aacc8b8f2
Dnl37 <dannymonge37@yahoo.com>
2022-04-28 01:05:58 -0700

    Init news controller

---
+++ Project/HomeView-API/Controllers/NewsController.cs
@@ -0,0 +1,44 @@
+﻿using Managers.Contracts;
+using Managers.Implementations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeView_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase {
+    
+        // GET: api/<NewsController>
+        [HttpGet]
+        public ActionResult<int> Get(int id)
+        {
+            return Ok(id);
+        }
+
+        // GET api/<NewsController>/
+        [HttpGet]
+        public ActionResult<string> Get(int id1, int id2)
+        {
+            int result = id1 + id2;
+            return Ok(result.ToString());
+        }
+
+        // POST api/<NewsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NewsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NewsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
