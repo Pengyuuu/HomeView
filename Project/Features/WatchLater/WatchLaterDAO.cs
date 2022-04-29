d59ecfc4d86b7888caaba7827e4d2d387010e9a5
Pengyuuu <erictruong15@gmail.com>
2022-04-29 00:16:14 -0700

    Working on watchlater

---
+++ Project/Features/WatchLater/WatchLaterDAO.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Data;
+
+namespace Features.WatchLater
+{
+    public class WatchLaterDAO
+    {
+        private readonly SqlDataAccess _db;
+
+        public WatchLaterDAO(SqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> AsyncAddToWatchLater(WatchLaterTitle selectedTitle)
+        {
+            var insertToList = new
+            {
+                email = selectedTitle.Email,
+                title = selectedTitle.Title,
+                year = selectedTitle.Year
+            };
+
+            int result = await _db.SaveData("dbo.WatchLater_AddToList", insertToList);
+
+            if (result != 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<bool> AsyncRemoveFromList(WatchLaterTitle selectedTitle)
+        {
+            var deleteFromList = new
+            {
+                email = selectedTitle.Email,
+                title = selectedTitle.Title,
+                year = selectedTitle.Year
+            };
+
+            int result = await _db.SaveData("dbo.WatchLater_RemoveFromList", deleteFromList);
+
+            if (result != 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<IEnumerable<WatchLaterTitle>> AsyncGetList(string userEmail)
+        {
+            var targetUser = new
+            {
+                email = userEmail
+            };
+
+            List<WatchLaterTitle> result = (List<WatchLaterTitle>) await _db.LoadData<WatchLaterTitle, dynamic>("dbo.WatchLater_GetList", targetUser);
+
+            if (result.Count == 0)
+            {
+                return null;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Project/Features/WatchLater/WatchLaterTitle.cs
@@ -0,0 +1,25 @@
+﻿namespace Features.WatchLater
+{
+    public class WatchLaterTitle
+    {
+        public string Email { get; set; }
+
+        public string Title { get; set; }
+
+        public string Year { get; set; }
+
+        public WatchLaterTitle()
+        {
+            Email = "";
+            Title = "";
+            Year = "";
+        }
+
+        public WatchLaterTitle(string email, string title, string year)
+        {
+            Email = email;
+            Title = title;
+            Year = year;
+        }
+    }
+}
