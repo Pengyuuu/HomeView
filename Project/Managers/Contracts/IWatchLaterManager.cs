18e97c275ac85e0de7780c13eb6c782bce5e51fa
Pengyuuu <erictruong15@gmail.com>
2022-04-29 12:37:20 -0700

    Watch Later DAO and service

---
+++ Project/Managers/Contracts/IWatchLaterManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Features.WatchLater;
+
+namespace Managers.Contracts
+{
+    public interface IWatchLaterManager
+    {
+        bool AddToWatchLater(string email, string title, string year);
+
+        bool RemoveFromList(string email, string title, string year);
+
+        List<WatchLaterTitle> GetList(string userEmail);
+    }
+}
