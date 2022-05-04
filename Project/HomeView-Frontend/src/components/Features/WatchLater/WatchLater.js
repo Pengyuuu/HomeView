991d826858c4e2d539e526592e931d6bbb854263
Eric Truong <erictruong15@gmail.com>
2022-05-04 16:41:11 -0700

    Watch later

--- Project/HomeView-Frontend/src/components/Features/WatchLater/WatchLater.js
+++ Project/HomeView-Frontend/src/components/Features/WatchLater/WatchLater.js
@@ -1,32 +1,30 @@
 import { Button } from "react-bootstrap";
-/* test*/
+import React from "react";
 
 export default function WatchLater() {
 
-    function AddToWatchLater() {
+    const AddToWatchLater = ({title, year}) => {
         const email = 'testing@gmail.com';
-        const title = 'Paranormal Activity';
-        const year = '2007';
 
-        const GET_URL = 'http://myhomeview.me/api/WatchLater/${email}/${title}/${year}';
+        const POST_URL = `http://myhomeview.me/api/WatchLater/${email}/${title}/${year}`;
 
-        fetch(GET_URL, {
+        fetch(POST_URL, {
 
-            method: 'GET',
+            method: 'POST',
 
             headers: { 'Content-Type': 'application/json' }
         }).then(res => {
             console.log(res);
 
             return res.json();
-        }).then(data => console.log("reach"))
+        })
 
         return true;
     }
 
     return (
         <div>
-            <Button className="mr-1" onClick={AddToWatchLater}>Watch Later</Button>
+            <Button onClick={AddToWatchLater}>Watch Later</Button>
         </div>
-    )
+    );
 }
