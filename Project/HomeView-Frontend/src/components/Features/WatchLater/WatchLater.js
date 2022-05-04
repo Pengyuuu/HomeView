6d68ab41c523ea57860eda110d26147cca56d7fa
Eric Truong <erictruong15@gmail.com>
2022-05-04 14:21:41 -0700

    navigation change

---
+++ Project/HomeView-Frontend/src/components/Features/WatchLater/WatchLater.js
@@ -0,0 +1,31 @@
+import { Button } from "react-bootstrap";
+
+export default function WatchLater() {
+
+    function AddToWatchLater() {
+        const email = 'testing@gmail.com';
+        const title = 'Paranormal Activity';
+        const year = '2007';
+
+        const GET_URL = 'http://myhomeview.me/api/WatchLater/${email}/${title}/${year}';
+
+        fetch(GET_URL, {
+
+            method: 'GET',
+
+            headers: { 'Content-Type': 'application/json' }
+        }).then(res => {
+            console.log(res);
+
+            return res.json();
+        }).then(data => console.log("reach"))
+
+        return true;
+    }
+
+    return (
+        <div>
+            <Button className="mr-1" onClick={AddToWatchLater}>Watch Later</Button>
+        </div>
+    )
+}
