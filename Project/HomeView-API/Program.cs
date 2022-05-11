78cb37d9a1f8265e14e77b9dd796fe406197a32c
Dnl37 <dannymonge37@yahoo.com>
2022-05-10 21:13:22 -0700

    environment dev set up

--- Project/HomeView-API/Program.cs
+++ Project/HomeView-API/Program.cs
@@ -1,8 +1,5 @@
 using Managers.Implementations;
 using Managers.Contracts;
-using System.Net.Http.Headers;
-using System.Web.Http;
-using Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var homeViewClient = "homeviewClient";
@@ -45,12 +42,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-if (app.Environment.IsProduction())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
